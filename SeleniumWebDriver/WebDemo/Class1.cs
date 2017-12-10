9c656d7f4a6ec8a498fea457e001350428e2c44f
RomanGM <xx.oo.2014@mail.ru>
2017-12-10 23:03:29 +0300

    rename2

---
+++ SeleniumWebDriver/WebDemo/Class1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDemo
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void oneCanOpenGoogle()
+        {
+            IWebDriver firefox = new FirefoxDriver();
+            firefox.Navigate().GoToUrl("https://github.com");
+
+            IWebElement signField = firefox.FindElement( By.XPath("//a[@href='/login']" ) );
+            signField.Click();
+
+            IWebElement loginField = firefox.FindElement( By.Name( "login" ) );
+            loginField.SendKeys( "testautomationuser");
+
+            IWebElement passwordField = firefox.FindElement( By.Name( "password" ) );
+            passwordField.SendKeys( "Time4Death!" );
+
+            IWebElement buttonSign = firefox.FindElement( By.Name( "commit" ) );
+            buttonSign.Click();
+        }
+    }
+}
